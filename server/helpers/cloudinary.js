10e125e75ee8621fa54f6b22ca61185adf06767f
dangnguyen2311 <nguyenquanghaidang2311@gmail.com>
2025-04-21 15:11:09 +0700

    remove some file

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -1,10 +1,11 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
+
 cloudinary.config({
-    cloud_name: "djrejurr6",
-    api_key: "611918842363449",
-    api_secret: "qyIwFcArxdWyqtPHANqYz6IZCG4",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
     mode: "sandbox",
-    client_id: "ARAHsxfchT6oPadYVgNnlSEBsvkFFUrYyv0J06I77k_r0juZhiUlM70Fq9mHHQwJ0q9Vz2WLh11zsG7g",
-    client_secret: "EFO-0RkDCaOIntVmT9-0CU_T6X46ZKQjWu0YS73wrQDvKQiRJolPlrrNeHTiTXOVGKC0NbAeyAeWSpEw",
+    client_id: process.env.PAYPAL_CLIENT_ID,
+    client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 module.exports = paypal;
